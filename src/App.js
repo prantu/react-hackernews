9e4fdfe91724d06c378a0bc31db31af6deeb0d4f
Tamal Sen <tamal.2k14@gmail.com>
2019-08-07 09:13:10 +0600

    Progress - 8/6/2019

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -13,35 +13,91 @@ const list = [
   {
     title: "Redux",
     author: "Dan Abramov",
-    num_comments: 8,
+    num_comments: 18,
     points: 2,
     objectID: 1
   }
 ];
 
-function App() {
-  let user = {
-    firstName: "Tamal",
-    lastName: "Sen"
-  };
+function isSearched(searchTerm) {
+  return function (item) {
+    // some conditions here
+  }
+}
 
-  const listItems = list.map((item => 
-    <div className="item-single">
-      <p>{item.title}</p>
-      <p>{item.author}</p>
-      <p>{item.num_comments}</p>
-      <p>{item.points}</p>
-    </div>
-  ));
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hey {user.firstName}! Here is your list.</p>
-        {listItems}
-      </header>
-    </div>
-  );
+//function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    let searchTerm = '';
+    /*     this.state = {
+          list: list,
+        } */
+    // Same as above 
+
+    this.state = {
+      list: list,
+      searchTerm: '',
+    }
+    // we won't need to use the bind method if we use onDismiss = (id) =>
+    //    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  // Class methods can be auto-bound using JavaScript ES6 arrow functions:
+  onDismiss = (id) => {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  /*   onDismiss(id) {
+      const updatedList = this.state.list.filter(item => item.objectID !== id);
+      this.setState({ list: updatedList });
+    }
+   */
+
+
+  render() {
+    let user = {
+      firstName: "Tamal",
+      lastName: "Sen"
+    };
+
+    const listItems = this.state.list.map(item =>
+      <div key={item.objectID}>
+        <p>{item.title}</p>
+        <p>{item.author}</p>
+        <p>{item.num_comments}</p>
+        <p>{item.points}</p>
+        <span>
+          <button onClick={() => this.onDismiss(item.objectID)}
+            // this.onDismiss(item.objectID) << This is best thing to use for now 
+            type="button"
+          >
+            Dismiss
+          </button>
+        </span>
+      </div>
+    );
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hey {user.firstName}! Here is your list.</p>
+          <form>
+            <input type="text" onChange={this.onSearchChange}/>
+          </form>
+          {listItems}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
